c7fb393ee9717aab141f5a35c6cb2cc1dffddb99
Nicolás Castellán <cnicolas.developer@gmail.com>
2022-11-11 17:50:56 -0300

    🔒 Upgrade from unwrap to expect
    This methods will now provide a better error message if they panic.
    Which they shouldn't.

--- src/file_ops.rs
+++ src/file_ops.rs
@@ -142,15 +142,15 @@ impl FileList {
 
             let source = source_list[index - offset]
                 .file_name()
-                .unwrap()
+                .expect("The program will have already ended if there's invalid UTF-8")
                 .to_str()
-                .unwrap()
+                .expect("The program will have already ended if there's invalid UTF-8")
                 .to_uppercase();
             let destination = destination_list[index]
                 .file_name()
-                .unwrap()
+                .expect("The program will have already ended if there's invalid UTF-8")
                 .to_str()
-                .unwrap()
+                .expect("The program will have already ended if there's invalid UTF-8")
                 .to_uppercase();
 
             if source != destination && destination_list[index].is_file() {
